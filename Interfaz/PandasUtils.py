6e2bbfdd5b50bc5847f3b34f0035b8bea4dc3a4e
Dumaru <robinsonmu@unisabana.edu.co>
2020-01-13 12:09:20 -0500

    Cambios carga, falta añadir threding

--- Interfaz/PandasUtils.py
+++ Interfaz/PandasUtils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def loadDataframes(pathList=[]):
     """
     Reads each excel file and saves all the sheets into a list with the specified columns
@@ -5,7 +8,7 @@ def loadDataframes(pathList=[]):
     dfs = []
     COLS = ["RAT","OPERATOR","CHANNEL","IMEI","IMSI","TMSI",
             "MS POWER","TA","LAST LAC","NAME","HITS","DATE-TIME"]
-    for filePath in filePaths:
+    for filePath in pathList:
         temd_dfs = [pd.read_excel(f"{filePath}", sheet_name=0, usecols=COLS), #2G
                     pd.read_excel(f"{filePath}", sheet_name=1, usecols=COLS), #3G
                     pd.read_excel(f"{filePath}", sheet_name=2, usecols=COLS)] #4G
