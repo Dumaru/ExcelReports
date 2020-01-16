502e18e7ec20d695e61fedd9b05e947faf947379
Dumaru <robinsonmu@unisabana.edu.co>
2020-01-15 20:27:35 -0500

    VentanaFiltros

--- Interfaz/VentanaFiltros.py
+++ Interfaz/VentanaFiltros.py
@@ -1,4 +1,8 @@
-from PyQt5.QtWidgets import QMainWindow
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.uic import loadUi
+import pandas as pd
+from PandasUtils import PandasDataLoader
 
 class VentanaFiltros(QMainWindow):
     def __init__(self, parent=None, pandasUtilsInstance=None):
@@ -7,7 +11,15 @@ class VentanaFiltros(QMainWindow):
         self.pandasUtils = pandasUtilsInstance if pandasUtilsInstance is not None else PandasDataLoader.getInstance()
         
         # UI
-    
+        loadUi('UI/VistaFiltros.ui', self)
+        self.setupUi()
+
     def setupUi(self):
         pass
-    
+
+
+if(__name__=="__main__"):
+    app = QApplication(sys.argv)
+    ventanaFiltros = VentanaFiltros()
+    ventanaFiltros.show()
+    sys.exit(app.exec())
